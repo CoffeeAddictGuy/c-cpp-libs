3a693ce08fbf1b1e39e710c4f539c8aaf8fc8601
CoffeeAddictGuy <richcrwo@gmail.com>
2025-12-23 14:06:02 +0300

    quick sort with lomuto partition

---
+++ algorithms/sorting/quick_sort/quick_sort.c
@@ -0,0 +1,54 @@
+#include "quick_sort.h"
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+void quick_sort(int *array, size_t size) { quicksort(array, 0, size - 1); }
+
+void quicksort(int *array, size_t left, size_t right) {
+  if (left >= right) {
+    return;
+  }
+
+  size_t pivot_idx = partition(array, left, right);
+  if (pivot_idx > left) {
+    quicksort(array, left, pivot_idx - 1);
+  }
+  quicksort(array, pivot_idx + 1, right);
+}
+
+size_t partition(int *array, size_t left, size_t right) {
+  size_t pivot = right;
+  size_t border = left;
+  for (size_t j = left; j < right; j++) {
+    if (array[j] < array[pivot]) {
+      swap(&array[border], &array[j]);
+      border++;
+    }
+  }
+  swap(&array[border], &array[pivot]);
+  return border;
+}
+
+void array_print(int *array, size_t size) {
+  printf("[");
+  for (size_t i = 0; i < size; i++) {
+    if (i != size - 1) {
+      printf("%d -> ", array[i]);
+    } else {
+      printf("%d]\n", array[i]);
+    }
+  }
+}
+
+bool is_sorted(int *array, size_t size) {
+  for (size_t i = 1; i < size; i++) {
+    if (array[i] < array[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ algorithms/sorting/quick_sort/quick_sort.h
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+void quick_sort(int *array, size_t size);
+void quicksort(int *array, size_t left, size_t right);
+size_t partition(int *array, size_t left, size_t right);
+void array_print(int *array, size_t size);
+bool is_sorted(int *array, size_t size);

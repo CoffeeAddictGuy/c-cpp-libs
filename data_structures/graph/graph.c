d301bb57a6dd9c0d41d011b6c90942eb6a56ddce
CoffeeAddictGuy <richcrwo@gmail.com>
2025-12-25 16:08:47 +0300

    add base graph realization

---
+++ data_structures/graph/graph.c
@@ -0,0 +1,49 @@
+#include "graph.h"
+
+Graph *graph_create(size_t num_vertices) {
+  Graph *g = (Graph *)malloc(sizeof(Graph));
+  g->num_vertices = num_vertices;
+  g->adjacency = ht_init(num_vertices * 2, HT_MODE_MULTIMAP);
+  return g;
+}
+
+void graph_add_edge(Graph *g, size_t from, size_t to) {
+  char c_from[32];
+  snprintf(c_from, sizeof(c_from), "%lld", from);
+  ht_insert(g->adjacency, c_from, to);
+}
+
+int graph_get_neighbors(Graph *g, size_t vertex, size_t *neighbors,
+                        size_t max_neighbors) {
+  char c_vertex[32];
+  snprintf(c_vertex, sizeof(c_vertex), "%lld", vertex);
+
+  unsigned int vertex_idx = hash_func(c_vertex, g->adjacency->capacity);
+  HashNode *curr = g->adjacency->buckets[vertex_idx];
+  size_t n_cnt = 0;
+  while (curr != NULL && n_cnt < max_neighbors) {
+    if (strcmp(curr->key, c_vertex) == 0) {
+      neighbors[n_cnt] = curr->value;
+      n_cnt++;
+    }
+    curr = curr->next;
+  }
+  printf("\n");
+  return n_cnt;
+}
+
+void graph_print(Graph *g) {
+  for (size_t i = 0; i < g->adjacency->capacity; i++) {
+    HashNode *curr = g->adjacency->buckets[i];
+    while (curr != NULL) {
+      printf("Bucket %lld: From %s to %d\n", i, curr->key, curr->value);
+      curr = curr->next;
+    }
+  }
+  printf("---\n");
+}
+
+void graph_free(Graph *g) {
+  ht_free(g->adjacency);
+  free(g);
+}
---
+++ data_structures/graph/graph.h
@@ -0,0 +1,13 @@
+#include "../hash_table_chain/ht_chaining.h"
+
+typedef struct {
+  HashTable *adjacency;
+  size_t num_vertices;
+} Graph;
+
+Graph *graph_create(size_t num_vertices);
+void graph_add_edge(Graph *g, size_t from, size_t to);
+int graph_get_neighbors(Graph *g, size_t vertex, size_t *neighbors,
+                        size_t max_neighbors);
+void graph_print(Graph *g);
+void graph_free(Graph *g);

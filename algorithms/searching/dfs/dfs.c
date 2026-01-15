fedfcf6dfcfd9758e1a9446dcdcdf6360b3e0a8b
CoffeeAddictGuy <richcrwo@gmail.com>
2026-01-15 16:22:19 +0300

    rewrite dfs

--- algorithms/searching/dfs/dfs.c
+++ algorithms/searching/dfs/dfs.c
@@ -3,37 +3,27 @@
 #define MAX_VERICES 10000
 
 DFSResult dfs(Graph *g, size_t source, size_t destination) {
+  DFSResult result = {false, NULL, NULL};
+  if (g == NULL) {
+    return result;
+  }
   DFSState state = {0};
-  DFSResult result = {0};
-
-  state.parent[source] = -1;
-  state.visited[source] = true;
-  dfs_recurcive(g, source, &state, &result, destination);
-  return result;
+  return *dfs_recurcive(g, source, &state, &result, destination);
 }
 
 DFSResult *dfs_recurcive(Graph *g, size_t v, DFSState *state, DFSResult *result,
                          size_t end) {
-  printf("Visit %zu\n", v);
-
+  size_t neighborns[10];
+  size_t n_cnt = graph_get_neighbors(g, v, neighborns, 10);
+  printf("Visiting vertex: %lld\n", v);
   if (v == end) {
-    printf("Found destination!\n");
-    printf("(size_t)-1 test - %lld\n", (size_t)-1);
     result->found = true;
-    size_t idx = 0;
-    size_t current = end;
-    while (current != ((size_t)-1)) {
-      result->path[idx++] = current;
-      current = state->parent[current];
-    }
-    result->path_length = idx;
+
     return result;
   }
 
-  size_t neigh[32];
-  size_t count = graph_get_neighbors(g, v, neigh, 32);
-  for (size_t i = 0; i < count; i++) {
-    size_t next = neigh[i];
+  for (size_t i = 0; i < n_cnt; i++) {
+    size_t next = neighborns[i];
     if (!state->visited[next]) {
       state->visited[next] = true;
       state->parent[next] = v;
@@ -43,5 +33,8 @@ DFSResult *dfs_recurcive(Graph *g, size_t v, DFSState *state, DFSResult *result,
       }
     }
   }
-  return result;
+
+  if (n_cnt < 1) {
+    return result;
+  }
 }
